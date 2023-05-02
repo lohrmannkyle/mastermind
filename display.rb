fe051ecc1200b74868dbf7ba3e13003746787c85
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-05-02 22:46:44 +0000

    Fix display class

--- display.rb
+++ display.rb
@@ -1,8 +1,7 @@
 class Display
     def self.intro
-        puts 
-        'Welcome to Mastermind! A game where the codemaker chooses four colors and the codebreaker attempts to guess the colors and order in which they were chosen. 
-        12 guesses are given.
-        Good luck and have fun!'
+        puts "\tWelcome to Mastermind! A game where the codemaker chooses four colors
+        and the codebreaker attempts to guess the colors and order in which they 
+        were chosen. 12 guesses are given. Good luck and have fun!"
     end
 end
