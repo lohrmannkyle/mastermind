925dc7f5abd51f104f61d90c3f222e39cdc90abd
Kyle Lohrmann <123435933+lohrmannkyle@users.noreply.github.com>
2023-05-03 16:47:32 +0000

    Add attributes to mastermind

--- display.rb
+++ display.rb
@@ -1,7 +1,13 @@
 class Display
     def self.intro
-        puts "\tWelcome to Mastermind! A game where the codemaker chooses four colors
-        and the codebreaker attempts to guess the colors and order in which they 
-        were chosen. 12 guesses are given. Good luck and have fun!"
+        puts "\nWelcome to Mastermind! A game where the codemaker chooses four colors"
+        puts 'and the codebreaker attempts to guess the colors and order in which they'
+        puts "were chosen. 12 guesses are given. Good luck and have fun!\n\n"
+        self.prompt_roles
+    end
+
+    def self.prompt_roles
+        puts "Would you like to be the codemaker or codebreaker?"
+        puts "Enter 1 for codemaker or 2 for codebreaker: "
     end
 end
--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,10 @@
 class Mastermind
     require_relative 'display'
+
+    def initialize
+        @codemaker = nil
+        @codebreaker = nil
+        @turn = nil
     def start
         Display.intro
     end
